3db83a80f46476dfd0abced1ce1b5b254c8a76bc
Natalia Ivantsova <ivantsovana@gmail.com>
2024-04-17 12:52:42 +0300

    feat: add header. apply page changing function

--- src/components/app/app.ts
+++ src/components/app/app.ts
@@ -1,10 +1,23 @@
+import { header } from '../header/header';
 import { garage } from '../garage/garage';
 import { winners } from '../winners/winners';
 
 export const app = (): HTMLElement => {
+    let mainPage = true;
+
+    function changePage(newValue: boolean) {
+        mainPage = newValue;
+        render();
+    }
+
+    function render() {
+        div.innerHTML = '';
+        div.appendChild(header(changePage));
+        mainPage ? div.appendChild(garage()) : div.appendChild(winners());
+    }
+
     const div = document.createElement('div');
-    div.appendChild(garage());
-    div.appendChild(winners());
+    render();
 
     return div;
 };
