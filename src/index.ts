a5d4f65469add8802a066bf90b88a6b4493f7d64
Natalia Ivantsova <ivantsovana@gmail.com>
2024-04-21 23:02:22 +0300

    create empty branck for mentors check pull request

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,7 @@
-import { app } from './components/app/app';
 import './style.css';
 
 const initializeApp = async () => {
-    const appElement = await app();
-    const bodyElement = document.querySelector('body');
-    if (bodyElement) {
-        bodyElement.appendChild(appElement);
-    } else {
-        console.error('Body element not found');
-    }
+   
 };
 
 initializeApp();
